0dc7791f9fd1f542e6f3e1a1ded4259aae5608d5
olubisiayantoye <olubisiayantoye@gmail.com>
2025-11-08 11:05:03 +0100

    modified:   Components/Layout/Admin/Sidebar.razor 	new file:   Components/Pages/Admin/Dashboard copy.razor 	modified:   obj/Debug/net9.0/BYUClinicCareApp.AssemblyInfo.cs 	modified:   obj/Debug/net9.0/BYUClinicCareApp.AssemblyInfoInputs.cache 	modified:   obj/Debug/net9.0/BYUClinicCareApp.GeneratedMSBuildEditorConfig.editorconfig

--- obj/Debug/net9.0/BYUClinicCareApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BYUClinicCareApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYUClinicCareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c03876616ccb571db4c2ef039000f5e2b7e078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9584e21751f6115d0dc74930fc34d0e08d8821c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYUClinicCareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYUClinicCareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
